949c9b3e871aedaa0650a833a9f981c9ecd64a0b
Abdur Rahman <rahmannnm24@gmail.com>
2026-03-09 10:33:43 +0530

    Changes in API

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"https://resort-bboking-backend.onrender.com"
+    baseURL:"https://api.natureheavenholidays.com"
 });
 
 export default API;
