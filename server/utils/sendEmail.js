a0397fed14f762d8b0f692ee935bdf7e8e19db42
Abdur Rahman <rahmannnm24@gmail.com>
2026-03-09 14:58:45 +0530

    Update in sendEmail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (to, subject, text) => {
   try {
     const response = await resend.emails.send({
-      from: "Nature Heaven <booking@natureheavebholidays.com>",
+      from: "Nature Heaven <booking@natureheavenholidays.com>",
       to: to,
       subject: subject,
       text: text,
